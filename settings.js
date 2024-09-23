d8f1768c8c17ee12204b40ff79a603253939220f
NobBot2AF <eonobdggg12@gmail.com>
2024-09-23 23:30:07 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,7 +19,7 @@ module.exports = {
 
 Qᴜᴇᴇɴ ᴇʟɪꜱᴀ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ`,
   ALIVE_MEDIA: 'https://telegra.ph/file/d842677f522b6bef4dfe6.jpg',
-  MONGODB_URL: '', 
+  MONGODB_URL: 'mongodb+srv://Ruwa:0326@ruwa.uk8yb.mongodb.net', 
   PREFIX: '.$&',
   LANGUAGE: 'EN',
   PORT: 3000,
